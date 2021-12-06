1ba6db411807188ad5a81ad99c40ccd2921012db
geshben <dl22geshl@gmail.com>
2021-12-06 16:12:33 +0300

    Intiliaze the Header

--- src/main.js
+++ src/main.js
@@ -2,16 +2,13 @@ import { createApp } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import App from './App.vue';
-import TeamsList from './components/teams/TeamsList.vue';
-import UsersList from './components/users/UsersList.vue';
+import TheNav from './components/TheNav.vue';
 
 const router = createRouter({
   history: createWebHistory(),
   routes: [
-    { path: '/teams', component: TeamsList }, // our-domain.com/teams => TeamsList
-    { path: '/users', component: UsersList },
-  ],
-  linkActiveClass: 'active'
+    { path: '/', component: TheNav }
+  ]
 });
 
 const app = createApp(App)
