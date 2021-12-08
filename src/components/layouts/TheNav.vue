aa6b5a51568ca125e2342e01299c71e3dbf62c95
geshben <dl22geshl@gmail.com>
2021-12-08 16:32:01 +0300

    Fix the nav styling

--- src/components/layouts/TheNav.vue
+++ src/components/layouts/TheNav.vue
@@ -1,8 +1,48 @@
 <template>
-    <header>
-        <h1>Help Earth To Breathe Free 🌲 For A Better Tomorrow</h1>
-    </header>
+  <header>
+    <h1>Help Earth To Breathe Free 🌲 For A Better Tomorrow</h1>
+  </header>
+  <base-card class="justfyBetween">
+    <base-button @click="setSelectedTab('trees-container')" :mode="treesMode">
+      <router-link to="/trees">Trees Already Exist</router-link>
+    </base-button>
+
+    <base-button @click="setSelectedTab('add-tree')" :mode="addTreeMode">
+      <router-link to="/addtree">Add new Trees </router-link>
+    </base-button>
+  </base-card>
 </template>
+
+
+<script>
+import BaseButton from '../UI/BaseButton.vue';
+import BaseCard from '../UI/BaseCard.vue';
+
+export default {
+  components: {
+    BaseButton,
+    BaseCard,
+  },
+  data() {
+    return {
+      selectedTab: 'trees-container',
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    },
+  },
+  computed: {
+    treesMode() {
+      return this.selectedTab === 'trees-container' ? null : 'flat';
+    },
+    addTreeMode() {
+      return this.selectedTab === 'add-tree' ? null : 'flat';
+    },
+  },
+};
+</script>
 
 <style scoped>
 header {
@@ -17,4 +57,15 @@ header h1 {
   color: white;
   margin: 0;
 }
+a{
+  color:#3a0061;
+text-decoration: none;
+
+}
+.justfyBetween
+{
+  display: flex;
+    justify-content: space-between;
+}
+
 </style>
--- src/router.js
+++ src/router.js
@@ -1,17 +1,17 @@
 import {createRouter,createWebHistory} from 'vue-router'
-import NotFoundPage from './pages/NotFoundPage'
-import TheTrees from './components/treeFiles/TheTrees.vue';
+import NotFoundPage from './pages/NotFoundPage';
 import TreesContainer from './components/treeFiles/TreesContainer.vue';
-
+import AddTree from './components/treeFiles/AddTree.vue';
+// import TheHeader from './components/layouts/TheNav.vue';
 
 const router = createRouter({
     history:createWebHistory(),
     routes:[    
         {
-            path:'/',component:TheTrees
+            path:'/trees',component:TreesContainer ,props:true
         },
         {
-            path:'/trees',component:TreesContainer
+            path:'/addtree',component:AddTree
         }
         ,{
             
